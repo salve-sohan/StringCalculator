9e608736de0968465f3d4fe8306e5e97a95181f4
sohan-salve <sohansalve04062002@gmail.com>
2025-07-09 23:52:59 +0530

    fix for step-6

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,15 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    parts = numbers.split(/,|\n/)
-    return parts.map(&:to_i).sum
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      custom_delim = parts[0][2..-1]
+      numbers = parts[1]
+      delimiter = Regexp.escape(custom_delim)
+    end
+
+    parts = numbers.split(/#{delimiter}/)
+    parts.map(&:to_i).sum
   end
 end

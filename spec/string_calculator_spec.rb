333b4380770ab86f48ac0bf1cfa30d83867e0e0a
sohan-salve <sohansalve04062002@gmail.com>
2025-07-09 23:35:50 +0530

    step-2 added one more spec with solution

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the given number if the number is one only' do
+      expect(StringCalculator.add("5")).to eq(5)
+    end
+
   end
 end

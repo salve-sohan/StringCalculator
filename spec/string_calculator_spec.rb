210cef790377b628ebaaeddc076f80764d0c064e
sohan-salve <sohansalve04062002@gmail.com>
2025-07-09 23:48:29 +0530

    step-5 wrote another test case with new line character

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,5 +18,8 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
 
+    it 'handles new line character' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
